f0f3b175262232e752568da1a0b9fba032782111
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-04 16:03:42 +0300

    test(parser): Add tests for function declarations, return, and calls

--- tests/Parser/ParserTest.php
+++ tests/Parser/ParserTest.php
@@ -7,11 +7,14 @@ namespace ChernegaSergiy\TrypilliaCompiler\Tests\Parser;
 use ChernegaSergiy\TrypilliaCompiler\Ast\AssignStmt;
 use ChernegaSergiy\TrypilliaCompiler\Ast\BinOpNode;
 use ChernegaSergiy\TrypilliaCompiler\Ast\BitNotNode;
+use ChernegaSergiy\TrypilliaCompiler\Ast\CallNode;
+use ChernegaSergiy\TrypilliaCompiler\Ast\FunctionDecl;
 use ChernegaSergiy\TrypilliaCompiler\Ast\IfStmt;
 use ChernegaSergiy\TrypilliaCompiler\Ast\LetStmt;
 use ChernegaSergiy\TrypilliaCompiler\Ast\NotNode;
 use ChernegaSergiy\TrypilliaCompiler\Ast\NumberNode;
 use ChernegaSergiy\TrypilliaCompiler\Ast\PrintStmt;
+use ChernegaSergiy\TrypilliaCompiler\Ast\ReturnStmt;
 use ChernegaSergiy\TrypilliaCompiler\Ast\StringNode;
 use ChernegaSergiy\TrypilliaCompiler\Ast\VarNode;
 use ChernegaSergiy\TrypilliaCompiler\Ast\WhileStmt;
@@ -228,5 +231,82 @@ class ParserTest extends TestCase
         $this->assertSame('|', $stmt->expr->op);
         $this->assertInstanceOf(BinOpNode::class, $stmt->expr->left);
         $this->assertSame('+', $stmt->expr->left->op);
+    }
+
+    public function testParsesFunctionDeclaration(): void
+    {
+        [$stmt] = $this->parse('fn add(a: i64, b: i64) -> i64 { return a + b; }');
+
+        $this->assertInstanceOf(FunctionDecl::class, $stmt);
+        $this->assertSame('add', $stmt->name);
+        $this->assertCount(2, $stmt->params);
+        $this->assertSame('a', $stmt->params[0]['name']);
+        $this->assertSame('i64', $stmt->params[0]['type']);
+        $this->assertSame('b', $stmt->params[1]['name']);
+        $this->assertSame('i64', $stmt->params[1]['type']);
+        $this->assertSame('i64', $stmt->returnType);
+        $this->assertCount(1, $stmt->body);
+        $this->assertInstanceOf(ReturnStmt::class, $stmt->body[0]);
+    }
+
+    public function testParsesFunctionWithNoParams(): void
+    {
+        [$stmt] = $this->parse('fn main() { print 42; }');
+
+        $this->assertInstanceOf(FunctionDecl::class, $stmt);
+        $this->assertSame('main', $stmt->name);
+        $this->assertCount(0, $stmt->params);
+        $this->assertSame('void', $stmt->returnType);
+        $this->assertCount(1, $stmt->body);
+    }
+
+    public function testParsesReturnWithExpression(): void
+    {
+        [$stmt] = $this->parse('fn f() -> i64 { return 42; }');
+
+        $this->assertInstanceOf(FunctionDecl::class, $stmt);
+        $returnStmt = $stmt->body[0];
+        $this->assertInstanceOf(ReturnStmt::class, $returnStmt);
+        $this->assertInstanceOf(NumberNode::class, $returnStmt->expr);
+        $this->assertSame(42, $returnStmt->expr->val);
+    }
+
+    public function testParsesReturnWithoutExpression(): void
+    {
+        [$stmt] = $this->parse('fn f() { return; }');
+
+        $this->assertInstanceOf(FunctionDecl::class, $stmt);
+        $returnStmt = $stmt->body[0];
+        $this->assertInstanceOf(ReturnStmt::class, $returnStmt);
+        $this->assertNull($returnStmt->expr);
+    }
+
+    public function testParsesFunctionCall(): void
+    {
+        [$stmt] = $this->parse('let x = add(1, 2);');
+
+        $this->assertInstanceOf(LetStmt::class, $stmt);
+        $this->assertInstanceOf(CallNode::class, $stmt->expr);
+        $this->assertSame('add', $stmt->expr->name);
+        $this->assertCount(2, $stmt->expr->args);
+        $this->assertInstanceOf(NumberNode::class, $stmt->expr->args[0]);
+        $this->assertInstanceOf(NumberNode::class, $stmt->expr->args[1]);
+    }
+
+    public function testParsesFunctionCallWithNoArgs(): void
+    {
+        [$stmt] = $this->parse('let x = main();');
+
+        $this->assertInstanceOf(CallNode::class, $stmt->expr);
+        $this->assertSame('main', $stmt->expr->name);
+        $this->assertCount(0, $stmt->expr->args);
+    }
+
+    public function testParsesFunctionCallInPrint(): void
+    {
+        [$stmt] = $this->parse('print add(1, 2);');
+
+        $this->assertInstanceOf(PrintStmt::class, $stmt);
+        $this->assertInstanceOf(CallNode::class, $stmt->expr);
     }
 }
